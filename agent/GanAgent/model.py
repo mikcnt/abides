3358b42d7828164b81ef0c998d7ffe807a9c8892
mikcnt <11929535+mikcnt@users.noreply.github.com>
2021-04-30 20:36:00 +0200

    Update model class

--- agent/GanAgent/model.py
+++ agent/GanAgent/model.py
@@ -1,35 +1,59 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class Generator(nn.Module):
-    def __init__(self, channels_noise, features_g=64):
-        super(Generator, self).__init__()
-        self.gen = nn.Sequential(
-            # Input: N x channels_noise x 1 x 1
-            self._block(channels_noise, features_g * 16, 4, 1, 0),  # img: 4x4
-            self._block(features_g * 16, features_g * 8, 4, 2, 1),  # img: 8x8
-            self._block(features_g * 8, features_g * 4, 4, 2, 1),  # img: 16x16
-            # self._block(features_g * 4, features_g * 2, 4, 2, 1),  # img: 32x32
-            nn.ConvTranspose2d(features_g * 4, 4, kernel_size=4, stride=2, padding=1),
-            # Output: N x 3 x 64 x 64
-        )
-        self.activation = nn.Tanh()
+    def __init__(self, input_channel):
+        super().__init__()
+        # input size: (batch_size, 12, 60)
+        # 12 = ohcl + noise
+        # 60 = rows from ohlc
+        # output size: (batch_size, 4, 60)
 
-    def _block(self, in_channels, out_channels, kernel_size, stride, padding):
-        return nn.Sequential(
-            nn.ConvTranspose2d(
-                in_channels,
-                out_channels,
-                kernel_size,
-                stride,
-                padding,
-                bias=False,
-            ),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(),
+        self.conv_blocks = nn.Sequential(
+            self._conv_block(input_channel, 128, 3, stride=2, padding=1),  # (128, 30)
+            self._conv_block(128, 256, 3, stride=2, padding=1),  # (256, 15)
+            self._conv_block(256, 256, 3, stride=1, padding=1),  # (256, 15)
         )
+
+        self.t_conv_blocks = nn.Sequential(
+            self._tconv_block(256, 128, 1, stride=1, padding=0),  # (128, 15)
+            self._tconv_block(128, 64, 4, stride=2, padding=1),  # (64, 30)
+            self._tconv_block(64, 12, 4, stride=2, padding=1),  # (12, 60)
+        )
+        self.last_conv = nn.ConvTranspose1d(12, 4, 3, stride=1, padding=1)  # (4, 60)
+
+        self.last_activation = nn.Tanh()
 
     def forward(self, x):
-        x = self.gen(x)
-        return self.activation(x) * 2
+        out = self.conv_blocks(x)
+        out = self.t_conv_blocks(out)
+        out = self.last_conv(out)
+        return self.last_activation(out) + 1
+
+    def _conv_block(self, in_channels, out_channels, kernel_size, stride, padding):
+        return nn.Sequential(
+            nn.Conv1d(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                kernel_size=kernel_size,
+                stride=stride,
+                padding=padding,
+            ),
+            nn.BatchNorm1d(out_channels),
+            nn.LeakyReLU(),
+        )
+
+    def _tconv_block(self, in_channels, out_channels, kernel_size, stride, padding):
+        return nn.Sequential(
+            nn.ConvTranspose1d(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                kernel_size=kernel_size,
+                stride=stride,
+                padding=padding,
+            ),
+            nn.BatchNorm1d(out_channels),
+            nn.LeakyReLU(),
+        )
